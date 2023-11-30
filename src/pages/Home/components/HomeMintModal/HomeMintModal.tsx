18706617770b1f618d643dcec774daf1fa3389ba
Jean Osorio <jeanosorio@Jeans-Mac-Studio.local>
2023-11-30 18:56:18 +0100

    feat: connect to metamask

--- src/pages/Home/components/HomeMintModal/HomeMintModal.tsx
+++ src/pages/Home/components/HomeMintModal/HomeMintModal.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, useState } from 'react';
-
+import { ReactNode, useState, useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
 import {
   HomeMintButtonsWrapperStyled,
   HomeMintFormWrapperStyled,
@@ -15,11 +16,21 @@ import SecondStep from './steps/SecondStep';
 import ThirdStep from './steps/ThirdStep';
 import ProgressBar from '../../../../components/ProgressBar/ProgressBar';
 import { useModal } from '../../../../context/ModalContext';
+import EtherContract from '../../../../contracts/EtherTea.json';
+
+const contractsNetworks = {
+  development: '1700473783318',
+  mainnet: '1',
+};
 
 export const HomeMintModal = () => {
   const [page, setPage] = useState(0);
   const [progress, setProgress] = useState(0);
   const { toggleModalVisibility } = useModal();
+  const [chainId, setChainId] = useState<string>('');
+  const [wallet, setWallet] = useState<string>('');
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [theContractInstance, setTheContractInstance] = useState<object | null>(null);
 
   const steps: Record<number, ReactNode> = {
     0: <FirstStep />,
@@ -31,11 +42,99 @@ export const HomeMintModal = () => {
     if (page < 2) {
       setPage(page + 1);
       setProgress(progress + 50);
+    } else if (page === 2) {
+      connectMetaMask();
     } else {
       setPage(0);
       toggleModalVisibility();
     }
   };
+
+  useEffect(() => {
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider();
+      console.log(provider);
+      if (provider) {
+        console.log('Ethereum successfully detected!');
+        // @ts-nocheck
+        // @ts-ignore
+        const chainId = await provider.request({
+          method: 'eth_chainId',
+        });
+        setChainId(chainId);
+      } else {
+        console.log('Please install/connect MetaMask!');
+      }
+    };
+    getProvider();
+  }, []);
+
+  useEffect(() => {
+    if (!window.ethereum && chainId === '') {
+      return;
+    }
+    createContractInstance();
+  }, [chainId]);
+
+  useEffect(() => {
+    if (!window.ethereum) {
+      return;
+    }
+    window.ethereum.on('accountsChanged', (accounts: string[]) => {
+      if (accounts[0]) {
+        setIsConnected(false);
+        setWallet(accounts[0]);
+      } else {
+        setIsConnected(false);
+      }
+      if (wallet !== accounts[0]) {
+        setIsConnected(false);
+      }
+    });
+
+    window.ethereum.on('chainChanged', async _chainId => {
+      setIsConnected(false);
+      setChainId(_chainId);
+    });
+  }, []);
+
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      const w3 = new Web3(window.ethereum);
+      try {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        if (accounts[0]) {
+          setWallet(accounts[0]);
+          setIsConnected(true);
+        }
+      } catch (error) {
+        throw new Error(String(error));
+      }
+    } else {
+      console.log('Please install MetaMask');
+    }
+  };
+
+  const createContractInstance = () => {
+    const w3 = new Web3(window.ethereum);
+    console.log(EtherContract.networks, chainId);
+    const deployedNetwork = EtherContract.networks[contractsNetworks.development];
+    const instance = new w3.eth.Contract(
+      EtherContract.abi,
+      deployedNetwork && deployedNetwork.address,
+    );
+    instance.options.address = EtherContract.networks[contractsNetworks.development].address;
+    console.log(instance);
+    setTheContractInstance(instance);
+  };
+
+  useEffect(() => {
+    if (isConnected && wallet) {
+      connectMetaMask();
+    }
+  }, []);
 
   return (
     <HomeMintWrapperStyled>
@@ -50,7 +149,7 @@ export const HomeMintModal = () => {
       <HomeMintFormWrapperStyled>{steps[page]}</HomeMintFormWrapperStyled>
       <HomeMintButtonsWrapperStyled>
         <Button size="small" onClick={nextStep}>
-          {page !== 2 ? 'NEXT' : 'CLOSE'}
+          {page !== 2 ? 'NEXT' : isConnected ? 'MINT' : 'CONNECT'}
         </Button>
       </HomeMintButtonsWrapperStyled>
     </HomeMintWrapperStyled>
